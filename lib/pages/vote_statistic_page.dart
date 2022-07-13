65d6c48ddfd889c68322e77e0eab42d0b804fc58
Muhammad Zubair <zubair.muhammad90@gmail.com>
2022-07-13 08:08:34 +0700

    modify vote statistic page

--- lib/pages/vote_statistic_page.dart
+++ lib/pages/vote_statistic_page.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class VoteStatisticPage extends StatelessWidget {
   const VoteStatisticPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("Vote Statistic Page"));
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: PieChart(
+        PieChartData(
+          sections: [
+            PieChartSectionData(
+              title: "Calon 1: 10 suara",
+              value: 10,
+              color: Colors.red,
+            ),
+            PieChartSectionData(
+              title: "Calon 2: 15 suara",
+              value: 15,
+              color: Colors.blue,
+            ),
+          ],
+        )
+      ),
+    );
   }
 }
